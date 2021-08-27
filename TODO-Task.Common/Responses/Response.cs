b3cba47d492c847914b74813f8c3bc7489816c61
duvan baena <djbpuerta@hotmail.com>
2021-08-27 13:09:16 -0500

    Update Todo

--- TODO-Task.Common/Responses/Response.cs
+++ TODO-Task.Common/Responses/Response.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TODO_Task.Common.Responses
+﻿namespace TODO_Task.Common.Responses
 {
-    public class Responses
+    public class Response
     {
         public bool IsSuccess { get; set; }
 
