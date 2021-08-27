74f64d786bbdd6e0c1b3aa86ee465a221be97ef5
duvan baena <djbpuerta@hotmail.com>
2021-08-27 18:06:41 -0500

    Unit test preparation

---
+++ TODO-Task.Test/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO_Task.Test.Helpers
+{
+    public enum LoggerTypes
+    {
+        NUll,
+        List
+    }
+}
